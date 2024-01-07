bffefdeeee70fe3d18ed8f292eba78a908b521d4
Awais Hyder <107742039+Awaishyderb@users.noreply.github.com>
2024-01-07 17:32:12 +0500

    created spring boot jpa tutorial project!

---
+++ spring-datajpa-tutorial/src/main/java/com/example/springdatajpatutorial/SpringDatajpaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpatutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatajpaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDatajpaTutorialApplication.class, args);
+	}
+
+}
---
+++ spring-datajpa-tutorial/src/test/java/com/example/springdatajpatutorial/SpringDatajpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatajpatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatajpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
