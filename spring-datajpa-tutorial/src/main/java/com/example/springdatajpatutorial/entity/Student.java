be8b1ff751eeeba68eacd73b4e958109873f97f0
Awais Hyder <107742039+Awaishyderb@users.noreply.github.com>
2024-01-09 14:17:10 +0500

    Created Student Entity

--- spring-datajpa-tutorial/src/main/java/com/example/springdatajpatutorial/entity/Student.java
+++ spring-datajpa-tutorial/src/main/java/com/example/springdatajpatutorial/entity/Student.java
@@ -1,2 +1,30 @@
-package com.example.springdatajpatutorial.entity;public class Student {
+package com.example.springdatajpatutorial.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity // It signals to the JPA provider that the class should be treated as a table in the database.
+@Data // Getters & Setters of Properties
+@AllArgsConstructor // set constructor with all properties
+@NoArgsConstructor // set constructor without properties
+@Builder // generate builder methods for classes.
+@Table(name = "student") // set TABLE properties
+public class Student {
+    @Id // PRIMARY KEY for tuple
+    private long studentId;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+
+
 }
