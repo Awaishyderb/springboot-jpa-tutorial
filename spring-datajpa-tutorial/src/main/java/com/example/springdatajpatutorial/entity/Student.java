903d30b7dc3c8d0622a22aaaa64461cb0456af2e
Awais Hyder <107742039+Awaishyderb@users.noreply.github.com>
2024-01-24 13:22:05 +0500

    modified Student entity

--- spring-datajpa-tutorial/src/main/java/com/example/springdatajpatutorial/entity/Student.java
+++ spring-datajpa-tutorial/src/main/java/com/example/springdatajpatutorial/entity/Student.java
@@ -1,8 +1,6 @@
 package com.example.springdatajpatutorial.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,13 +13,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor // set constructor with all properties
 @NoArgsConstructor // set constructor without properties
 @Builder // generate builder methods for classes.
-@Table(name = "student") // set TABLE properties
+@Table(
+        name = "student"
+) // set TABLE properties
 public class Student {
-    @Id // PRIMARY KEY for tuple
+    @Id // PRIMARY KEY for column
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto generates ID
     private long studentId;
     private String firstName;
     private String lastName;
+    @Column(name = "email_id") // change column name
     private String emailId;
+
     private String guardianName;
     private String guardianEmail;
     private String guardianMobile;
